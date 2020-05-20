a7a4be95ca9d1a292bb2078ab1223f3732d7078b
jayveebasco <jayveebasco.jb@gmail.com>
2020-05-20 21:47:12 +0800

    Initial commit

---
+++ src/containers/home.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+import '../stylesheets/main.scss'
+
+export default class Home extends Component {
+  render() {
+    return <div>This is Home page</div>
+  }
+}
